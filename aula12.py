9321f40ff0bbb6bbd8317792ccc6aabb56a60e19
Joel Paes <jodenis@outlook.com>
2024-09-13 02:07:58 -0300

    atualizando alguns comentários

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = 'Joel Jodenis Paes Pereira'
+alturas = 1.80
+peso = 95
+imc = peso / alturas ** 2
+
+print(nome, 'tem', alturas, 'de altura',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
---
+++ aula7.py
@@ -0,0 +1,12 @@
+# nome_completo = 'Joel Jodenis Paes Pereira'
+# int_um = int('1')
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(nome_completo) #Joel Jodenis Paes Pereira
+# print(soma_dois_mais_dois)
+# print(int_um, type(int_um))
+nome = 'luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
