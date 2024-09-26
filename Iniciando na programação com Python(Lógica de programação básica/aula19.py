cc1a1c06a619c8150d36d85c50a77cf0eb02192e
Joel Paes <jodenis@outlook.com>
2024-09-26 01:00:38 -0300

    comentada

--- Iniciando na programação com Python(Lógica de programação básica/aula19.py
+++ Iniciando na programação com Python(Lógica de programação básica/aula19.py
@@ -8,10 +8,10 @@ OP      Significado         Exemplo (True)
 ==      igual               'a' == 'a'
 !=      diferente           'a' != 'b'
 """
-maior = 2 > 1
-maior_ou_igual = 2 >= 2
-menor = 1 < 2
-menor_ou_igual = 2 <= 2
-igual = 'a' == 'a'
-diferente = 'a' != 'b'
-print('Olha meu print aqui')
+maior = 2 > 1  # Verifica se 2 é maior que 1. O resultado será True, pois 2 é maior que 1.
+maior_ou_igual = 2 >= 2  # Verifica se 2 é maior ou igual a 2. O resultado será True, pois 2 é igual a 2.
+menor = 1 < 2  # Verifica se 1 é menor que 2. O resultado será True, pois 1 é menor que 2.
+menor_ou_igual = 2 <= 2  # Verifica se 2 é menor ou igual a 2. O resultado será True, pois 2 é igual a 2.
+igual = 'a' == 'a'  # Verifica se a string 'a' é igual a 'a'. O resultado será True, pois ambas são idênticas.
+diferente = 'a' != 'b'  # Verifica se a string 'a' é diferente de 'b'. O resultado será True, pois as duas strings são diferentes.
+print('Olha meu print aqui')  # Exibe a mensagem 'Olha meu print aqui' no console.
