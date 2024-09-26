6f1888c7d69eb6e433bf691e0f0f8a788148a97a
Joel Paes <jodenis@outlook.com>
2024-09-26 01:54:19 -0300

    realocando

--- aula19.py
+++ aula19.py
@@ -14,4 +14,4 @@ menor = 1 < 2  # Verifica se 1 é menor que 2. O resultado será True, pois 1 é
 menor_ou_igual = 2 <= 2  # Verifica se 2 é menor ou igual a 2. O resultado será True, pois 2 é igual a 2.
 igual = 'a' == 'a'  # Verifica se a string 'a' é igual a 'a'. O resultado será True, pois ambas são idênticas.
 diferente = 'a' != 'b'  # Verifica se a string 'a' é diferente de 'b'. O resultado será True, pois as duas strings são diferentes.
-print('Olha meu print aqui')  # Exibe a mensagem 'Olha meu print aqui' no console.
+print(maior)  # Exibe a mensagem 'Olha meu print aqui' no console.
